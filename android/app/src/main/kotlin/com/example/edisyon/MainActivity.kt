4ebfabce6dae5ec76f595f1057b19058aace17d4
EminAkkaya <akkaya.emin.muhammed@gmail.com>
2026-02-14 23:37:39 +0300

    I created new app. I forget much thing but I hope i remamber quickly.

---
+++ android/app/src/main/kotlin/com/example/edisyon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.edisyon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/ui/landing/landingView.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Landingview extends StatelessWidget {
+  const Landingview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Welcome Back My Old Friend\nI'm Very Happy To See You"),
+    );
+  }
+}
