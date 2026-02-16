078b2130068b94fc3bf98381fd1f39a0a62af51e
EminAkkaya <akkaya.emin.muhammed@gmail.com>
2026-02-16 19:40:55 +0300

    For remamber the basics i made basic counter. Beside I need this logic for order counts.

--- lib/ui/landing/landingView.dart
+++ lib/ui/landing/landingView.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 
-class Landingview extends StatelessWidget {
+class Landingview extends StatefulWidget {
   const Landingview({super.key});
 
   @override
+  State<Landingview> createState() => _LandingviewState();
+}
+
+class _LandingviewState extends State<Landingview> {
+  int count = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Welcome Back My Old Friend\nI'm Very Happy To See You"),
+    return Column(
+      children: [
+        Text("Current Number :$count"),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              onPressed: () {
+                setState(() {
+                  count++;
+                });
+              },
+              label: Text("Ekle"),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                setState(() {
+                  count--;
+                });
+              },
+              label: Text("Azalt"),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
